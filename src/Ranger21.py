100e07d81608b92e525c592292df14af9aff7cb5
Less Wright <lessw@etrillium.com>
2021-04-04 21:18:55 -0700

    starting imports

--- src/Ranger21.py
+++ src/Ranger21.py
@@ -1,1 +1,21 @@
 # Ranger21 - @lessw2020
+
+# core components based on:
+
+# MADGRAD: https://arxiv.org/abs/2101.11075
+
+# warmup:  https://arxiv.org/abs/1910.04209v3
+
+# stable weight decay: https://arxiv.org/abs/2011.11152v3
+
+# Gradient Centralization: https://arxiv.org/abs/2004.01461v2
+
+
+import torch
+import torch.optim
+
+import math
+import collections
+
+import copy
+from torch import linalg as LA
